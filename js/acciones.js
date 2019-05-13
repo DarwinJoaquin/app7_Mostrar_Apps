902c3558557a4e99f005b3d62b011eeca4732b08
DarwinJoaquin <darwinjoaquinalarconbriones@gmail.com>
2019-05-13 13:58:35 -0500

    3

--- js/acciones.js
+++ js/acciones.js
@@ -24,4 +24,18 @@ $(document).ready(function (e){
 		});
 	},false);
 });
-					
+			$(document).ready(function(e){
+	document.addEventListener("deviceready" ,function(){
+		
+	$('#beep').tap(function(){
+		navigator.notification.beep(1);
+	});
+	
+	$('#vibrar').tap(function(){
+		navigator.notification.vibrate(1000);
+	});
+	
+	},false);
+});
+
+		
